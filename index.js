d43596dd4706a329e8abc1301f07f11217807107
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-06-01 00:05:58 +0600

    added delete api for coffees

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete(`/coffees/:id`, async(req, res)=>{
+      const id = req.params.id;
+      
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
+
+      res.send(result)
+    })
 
   } finally {
     // await client.close();
