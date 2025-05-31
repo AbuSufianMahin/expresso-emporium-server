4965015a10a6c057a3ead457a5966bca57990f91
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-05-31 23:14:21 +0600

    added API to edit(put method) coffee details

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send("Expresso Emporium Coffee Shop is running");
+app.get('/', (req, res) => {
+  res.send("Expresso Emporium Coffee Shop is running");
 })
 
 const uri = `mongodb+srv://${process.env.EXPRESSO_MONGO_ADMIN_NAME}:${process.env.EXPRESSO_MONGO_ADMIN_PASS}@cluster0.udgfocl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -35,26 +35,43 @@ async function run() {
 
     const coffeeCollection = client.db('coffeeDB').collection('coffees')
 
-    app.get('/coffees', async(req, res)=>{
-        const allCoffeeData = await coffeeCollection.find().toArray();
-        res.send(allCoffeeData);
+    app.get('/coffees', async (req, res) => {
+      const allCoffeeData = await coffeeCollection.find().toArray();
+      res.send(allCoffeeData);
     })
 
-    app.get('/coffees/:id', async(req, res)=>{
+    app.get('/coffees/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.findOne(query);
 
       res.send(result);
 
     })
 
-    app.post('/coffees', async(req, res)=>{
-        const coffeeData = req.body;
-        // console.log(req.body)
-        const result = await coffeeCollection.insertOne(coffeeData);
+    app.post('/coffees', async (req, res) => {
+      const coffeeData = req.body;
+      const result = await coffeeCollection.insertOne(coffeeData);
 
-        res.send(result)
+      res.send(result)
+    })
+
+
+    app.put('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+
+      // getting the matching collection (query)
+      const query = { _id: new ObjectId(id) };
+
+      const updatedData = req.body;
+
+      const updateDoc = {
+        $set: updatedData
+      }
+
+      const result = await coffeeCollection.updateOne(query, updateDoc);
+
+      res.send(result);
     })
 
 
@@ -67,6 +84,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port, ()=>{
-    console.log("Expresso Emporium server running on port: ", port);
+app.listen(port, () => {
+  console.log("Expresso Emporium server running on port: ", port);
 })
